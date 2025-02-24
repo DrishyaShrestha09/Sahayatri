cfeb0afc805e810f78ce8dd519bbf16f0274341f
Ridan Shrestha <ridanstha09@gmail.com>
2025-02-24 09:48:06 +0545

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='container '>
+        <div className="child bg-amber-400">
+        <h1 className='mt-10  flex justify-center font-bold text-amber-950 '>Hello world</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
