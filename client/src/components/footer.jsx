347583a85789a34b0bf93aebffc4658d93b696c3
Ridan Shrestha <ridanstha09@gmail.com>
2025-02-27 10:27:38 +0545

    added structure for footer

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,10 +1,23 @@
-import '../index.css';
+import "../index.css";
+import { LuSendHorizontal } from "react-icons/lu";
 const Footer = () => {
   return (
-    <div>
-      <h1>This is a footer</h1>
+    <div className="main bg-white">
+      <div className="header">
+        <h1>Sahayatri</h1>
+        <p>Give, Change, Inspire </p>
+      </div>
+      <div className="btn bg-[#008080]">
+        Donate Now
+        <div className="icon bg-[#A8E067]">
+          <LuSendHorizontal />
+        </div>
+      </div>
+      <div className="rights text-left text-xs text-gray-600 px-4 py-2">
+        <p>&copy; 2025 Sahayatri. All rights reserved.</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
