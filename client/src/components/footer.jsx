afabfb9d99903a45d4b4d6a56002a18a488c408a
Ridan Shrestha <ridanstha09@gmail.com>
2025-02-28 23:20:41 +0545

    added some contents in about us page

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -14,58 +14,57 @@ const Footer = () => {
         <div className="icon bg-[#A8E067] rounded-[20px] ">
           <LuSendHorizontal className="m-1 " />
         </div>
-      </div>  
+      </div>
       {/* Creating copy-right*/}
       <div className="rights text-left  text-black text-xl px-4 py-2 mt-15">
         <p>&copy; 2025 Sahayatri. All rights reserved.</p>
       </div>
       {/* Creating main container for quick links*/}
-        <div className="manage justify-end w-full flex flex-wrap gap-40 -mx-10">
-          <div className="quick-links -my-65 ">
-            <h3 className="font-semibold text-lg mb-2 ">Quick Links</h3>
-            <ul className="space-y-5 mt-5">
-              <li>
-                <Link to="/" className="hover:underline">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link to="/campaign" className="hover:underline">
-                  Campaign
-                </Link>
-              </li>
-              <li>
-                <Link to="/about" className="hover:underline">
+      <div className="manage justify-end w-full flex flex-wrap gap-40 -mx-10">
+        <div className="quick-links -my-65 ">
+          <h3 className="font-semibold text-lg mb-2 ">Quick Links</h3>
+          <ul className="space-y-5 mt-5">
+            <li>
+              <Link to="/" className="hover:underline">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/campaign" className="hover:underline">
+                Campaign
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className="hover:underline">
                 About Us
-                </Link>
-              </li>
-              <li>
-                <Link to="/contact" className="hover:underline">
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="social -my-65 px-10">
-            <h3 className="font-semibold text-lg mb-2">Socials</h3>
-            <ul className="space-y-5 mt-5">
-              <li>
-                <a href="#">Facebook</a>
-              </li>
-              <li>
-                <a href="#">Instagram</a>
-              </li>
-              <li>
-                <a href="#">Twitter (X)</a>
-              </li>
-              <li>
-                <a href="#">LinkedIn</a>
-              </li>
-            </ul>
-          </div>
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" className="hover:underline">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="social -my-65 px-10">
+          <h3 className="font-semibold text-lg mb-2">Socials</h3>
+          <ul className="space-y-5 mt-5">
+            <li>
+              <a href="#">Facebook</a>
+            </li>
+            <li>
+              <a href="#">Instagram</a>
+            </li>
+            <li>
+              <a href="#">Twitter (X)</a>
+            </li>
+            <li>
+              <a href="#">LinkedIn</a>
+            </li>
+          </ul>
         </div>
       </div>
-    
+    </div>
   );
 };
 
--- client/src/pages/about_us.jsx
+++ client/src/pages/about_us.jsx
@@ -1,8 +1,16 @@
 import '../index.css';
 const About_us = () => {
   return (
-    <div>
-      <h1>this is about us page</h1>
+    <div className='main'>
+      <div className="into">
+        <h1>Introduction to Sahayatri</h1>
+        <p>Sahayatri is more than just a crowdfunding platform it's a movement that connects dreamers with believers. Whether it's for social causes, business ventures, or personal projects, we provide a transparent and secure space where ideas come to life through the power of collective support.</p>
+      </div>
+      <div className="mission">
+        <h1>Our Mission</h1>
+        <p>Our mission is simple: to empower individuals and organizations by providing a seamless and trustworthy platform to raise funds. We believe in the power of community and collaboration to make dreams a reality.</p>
+      </div>
+      
     </div>
   )
 }
