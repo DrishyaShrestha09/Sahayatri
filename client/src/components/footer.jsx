21ee7439bfd411eca55386182f499f7c018fdaac
Ridan Shrestha <ridanstha09@gmail.com>
2025-02-28 21:39:24 +0545

    fixed footer and added styling to it

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -3,39 +3,69 @@ import { LuSendHorizontal } from "react-icons/lu";
 import { Link } from "react-router-dom";
 const Footer = () => {
   return (
-    <div className="main bg-white">
-      <div className="header">
-        <h1 className="font-bold text-2xl">Sahayatri</h1>
-        <p>Give, Change, Inspire. </p>
+    <div className="main bg-white p-10 m-5 mt-10 rounded-[20px] h-120 w-370">
+      <div className="header m-10">
+        <h1 className="font-bold text-3xl">Sahayatri</h1>
+        <p className="mt-5 text-xl">Give, Change, Inspire. </p>
       </div>
-      <div className="btn bg-[#008080]">
-        Donate Now
-        <div className="icon bg-[#A8E067]">
-          <LuSendHorizontal />
+      {/* Creating button*/}
+      <div className="btn bg-[#008080] m-5 flex gap-4 h-10 w-60 justify-center p-2 rounded-[20px] cursor-pointer hover:bg-[#006666]">
+        <h1 className="text-white ">Donate Now</h1>
+        <div className="icon bg-[#A8E067] rounded-[20px] ">
+          <LuSendHorizontal className="m-1 " />
         </div>
-      </div>
-      <div className="rights text-left text-xs text-gray-600 px-4 py-2">
+      </div>  
+      {/* Creating copy-right*/}
+      <div className="rights text-left  text-black text-xl px-4 py-2 mt-15">
         <p>&copy; 2025 Sahayatri. All rights reserved.</p>
       </div>
-      <div className="quick-links">
-      <h3 className="font-semibold text-lg mb-2">Quick Links</h3>
-          <ul className="space-y-1">
-            <li><Link to="/" className="hover:underline">Home</Link></li>
-            <li><Link to="/about" className="hover:underline">About Us</Link></li>
-            <li><Link to="/campaigns" className="hover:underline">Campaigns</Link></li>
-            <li><Link to="/contact" className="hover:underline">Contact</Link></li>
-          </ul>
+      {/* Creating main container for quick links*/}
+        <div className="manage justify-end w-full flex flex-wrap gap-40 -mx-10">
+          <div className="quick-links -my-65 ">
+            <h3 className="font-semibold text-lg mb-2 ">Quick Links</h3>
+            <ul className="space-y-5 mt-5">
+              <li>
+                <Link to="/" className="hover:underline">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/campaign" className="hover:underline">
+                  Campaign
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" className="hover:underline">
+                About Us
+                </Link>
+              </li>
+              <li>
+                <Link to="/contact" className="hover:underline">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="social -my-65 px-10">
+            <h3 className="font-semibold text-lg mb-2">Socials</h3>
+            <ul className="space-y-5 mt-5">
+              <li>
+                <a href="#">Facebook</a>
+              </li>
+              <li>
+                <a href="#">Instagram</a>
+              </li>
+              <li>
+                <a href="#">Twitter (X)</a>
+              </li>
+              <li>
+                <a href="#">LinkedIn</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className="social">
-        <h3 className="font-semibold text-lg m-2">Socials</h3>
-          <ul className="space-x-4">
-            <li><a href="#">Facebook</a></li>
-            <li><a href="#">Instagram</a></li>
-            <li><a href="#">Twitter (X)</a></li>
-            <li><a href="#">LinedIn</a></li>
-          </ul>
-      </div>
-    </div>
+    
   );
 };
 
