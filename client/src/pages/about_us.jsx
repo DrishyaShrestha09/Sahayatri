4f1be0048572ddc7915cd0e2bf34235909158065
Ridan Shrestha <ridanstha09@gmail.com>
2025-03-02 22:42:04 +0545

    madae some changees

--- client/src/pages/about_us.jsx
+++ client/src/pages/about_us.jsx
@@ -10,10 +10,7 @@ const About_us = () => {
         <h1>Our Mission</h1>
         <p>Our mission is simple: to empower individuals and organizations by providing a seamless and trustworthy platform to raise funds. We believe in the power of community and collaboration to make dreams a reality.</p>
       </div>
-      <div>
-        <h1>Our Team</h1>
-        <p>Sahayatri is built by a diverse team of dedicated individuals. We have a strong focus on empowering individuals to make their dreams a reality and fostering a sense of belonging for all.</p>
-      </div>
+      
     </div>
   )
 }
