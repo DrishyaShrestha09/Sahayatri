113e7f2196bab398cfc58694c2750a2c76e7fb7f
Ridan Shrestha <ridanstha09@gmail.com>
2025-03-07 23:07:14 +0545

    added form components

--- client/src/pages/contact.jsx
+++ client/src/pages/contact.jsx
@@ -2,7 +2,31 @@ import '../index.css';
 const Contact = () => {
   return (
     <div>
-      <h1>this is contact page</h1>
+      <div className="form">
+        <h2>Contact Us</h2>
+        <form>
+          <div className="form-group">
+            <label for="name">Name</label>
+            <input type="text" className="form-control" id="name" required />
+          </div>
+          <div className="form-group">
+            <label for="email">Email</label>
+            <input type="email" className="form-control" id="email" required />
+          </div>
+          <div className="form-group">
+            <label for="message">Message</label>
+            <textarea
+              className="form-control"
+              id="message"
+              rows="3"
+              required
+            ></textarea>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
