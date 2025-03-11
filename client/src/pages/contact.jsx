653e04e569cde858b36e8263f0be819a2835d685
Ridan Shrestha <ridanstha09@gmail.com>
2025-03-11 18:49:00 +0545

    added footer sec

--- client/src/pages/contact.jsx
+++ client/src/pages/contact.jsx
@@ -1,32 +1,13 @@
 import '../index.css';
+import Footer from "../components/Footer";
 const Contact = () => {
   return (
     <div>
       <div className="form">
         <h2>Contact Us</h2>
-        <form>
-          <div className="form-group">
-            <label for="name">Name</label>
-            <input type="text" className="form-control" id="name" required />
-          </div>
-          <div className="form-group">
-            <label for="email">Email</label>
-            <input type="email" className="form-control" id="email" required />
-          </div>
-          <div className="form-group">
-            <label for="message">Message</label>
-            <textarea
-              className="form-control"
-              id="message"
-              rows="3"
-              required
-            ></textarea>
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>
+
       </div>
+      <Footer />
     </div>
   )
 }
