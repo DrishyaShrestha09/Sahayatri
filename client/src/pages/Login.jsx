ca5af0215aaa5e896dc9a9d2b0bd767e3fe42ca8
Ridan Shrestha <ridanstha09@gmail.com>
2025-03-11 21:53:13 +0545

    added login and registeration

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,35 @@
 const Login = () => {
   return (
-    <div>
-      <h1>this is login</h1>
+    <div className="flex justify-center align-middle mt-20 ">
+      <div className="w-[350px] h-[500px] bg-white shadow-lg rounded-lg p-6 ">
+        <p className="text-center text-2xl font-extrabold mb-6">Welcome back</p>
+        <form className="flex flex-col gap-4 mb-4">
+          <input
+            type="email"
+            className="rounded-full border border-gray-400 outline-none p-3"
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            className="rounded-full border border-gray-400 outline-none p-3"
+            placeholder="Password"
+          />
+          <p className="text-right text-gray-500 underline text-xs font-bold cursor-pointer hover:text-black">
+            Forgot Password?
+          </p>
+          <button className="p-3 rounded-full bg-teal-700 text-white font-bold shadow-md hover:shadow-none">
+            Log in
+          </button>
+        </form>
+        <p className="text-xs text-gray-500 text-center">
+          Don't have an account?{" "}
+          <span className="text-teal-700 font-extrabold underline cursor-pointer">
+            Sign up
+          </span>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,42 @@
-import '../index.css';
 const Register = () => {
   return (
-    <div>
-     <div className="main">
-       
-     </div>
+    <div className="flex justify-center align-middle mt-20">
+      <div className="w-[350px] h-[550px] bg-white shadow-lg rounded-lg p-6">
+        <p className="text-center text-2xl font-extrabold mb-4">
+          Create account
+        </p>
+        <p className="text-xs text-center mb-4">
+          Let's get started with your 30 days free trial
+        </p>
+        <form className="flex flex-col gap-4 mb-4">
+          <input
+            type="text"
+            className="rounded-full border border-gray-400 outline-none p-3"
+            placeholder="Name"
+          />
+          <input
+            type="email"
+            className="rounded-full border border-gray-400 outline-none p-3"
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            className="rounded-full border border-gray-400 outline-none p-3"
+            placeholder="Password"
+          />
+          <button className="p-3 rounded-full bg-teal-700 text-white font-bold shadow-md hover:shadow-none">
+            Create account
+          </button>
+        </form>
+        <p className="text-xs text-gray-500 text-center">
+          Already have an account?{" "}
+          <span className="text-teal-700 font-extrabold underline cursor-pointer">
+            Log in
+          </span>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
