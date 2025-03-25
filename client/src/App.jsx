74b11f909334560bb7e2b4f3cc09a102965f242e
Ridan Shrestha <ridanstha09@gmail.com>
2025-03-25 15:19:04 +0545

    added login and register using firebase

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
 import { Outlet } from "react-router-dom";
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+import { AuthProvider } from "./context/AuthContext";
 
 const App = () => {
   return (
     <>
-      <Navigation />
-      <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6">
-      <Outlet /> 
-      </main>
-      <Footer />
+      <AuthProvider>
+        <Navigation />
+        <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6">
+          <Outlet />
+        </main>
+        <Footer />
+      </AuthProvider>
     </>
   );
 };
